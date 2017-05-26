2fddfa8beae78dfb41db1e360db6942f5df4c122
Ben Dodd <spiritscollide@gmail.com>
2017-05-26 14:54:23 +0100

    Created setup.py
    Setup script to install TopograPy

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='topograpy',
+      version='0.1',
+      description='Coordinate Generator for Height Maps',
+      url='http://github.com/mitgobla/TopograPy',
+      author='Ben Dodd',
+      author_email='ben-dodd@outlook.com',
+      license='MIT',
+      packages=['topograpy'],
+      install_requires=[
+          'pillow',
+      ],
+      zip_safe=False)
