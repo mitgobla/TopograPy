ec1bee0b18dd0bc6a78934a4531f17ffac456fcc
Ben Dodd <spiritscollide@gmail.com>
2018-01-14 15:30:55 +0000

    Update sectioncalculation.py

--- topograpy/sectioncalculation.py
+++ topograpy/sectioncalculation.py
@@ -29,7 +29,7 @@ class SectionCalculation:
         """Calculates the highest point and lowest"""
         for rows in range(len(self.topography_darkness)):
             pin_high = max(self.topography_darkness[rows])
-            if pin_high > self.highest:
+            if pin_high[0] > self.highest:
                 self.highest = pin_high
 
         for rows in range(len(self.topography_darkness)):
